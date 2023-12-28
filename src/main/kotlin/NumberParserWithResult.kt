5d9ddaf2927bab7cd78c23928248b64e63c020bd
Daniel Tiefenauer <tiefenauer@mptechnology.ch>
2023-12-28 09:48:39 +0100

    add examples for Result

---
+++ src/main/kotlin/NumberParserWithResult.kt
@@ -0,0 +1,34 @@
+package org.example
+
+import arrow.core.flatMap
+import kotlin.math.ceil
+
+object NumberParserWithResult {
+    /**
+     * Converts a string-representation of a number to an int.
+     * Fractional values will always be rounded up.
+     * Both '.' and ',' are accepted as separators for the fractional part
+     * @throws IllegalArgumentException if the string-representation of the int is not within the interval [Int.MIN_VALUE, Int.MAX_VALUE]
+     * @throws NumberFormatException if the string is not a valid string-representation of a number
+     */
+    fun toInt(string: String): Result<Int> {
+        val intResult = runCatching { string.toInt() }
+        if (intResult.isSuccess) {
+            return intResult
+        }
+        return runCatching { string.toDouble() }
+            .recoverCatching { string.replace(',', '.').toDouble() }
+            .flatMap {
+                kotlin.runCatching {
+                    it.checkValue(string)
+                }
+            }
+            .map { ceil(it).toInt() }
+    }
+
+    private fun Double.checkValue(str: String) = when {
+        this > Int.MAX_VALUE -> throw IllegalArgumentException("$str is invalid: string value cannot be greater than ${Int.MAX_VALUE}")
+        this < Int.MIN_VALUE -> throw IllegalArgumentException("$str is invalid: string value cannot be less than ${Int.MIN_VALUE}")
+        else -> this
+    }
+}
---
+++ src/test/kotlin/NumberParserWithResultTest.kt
@@ -0,0 +1,68 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.example.NumberParserWithResult
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class NumberParserWithResultTest {
+    @Test
+    fun `should parse an int value`() {
+        val result = NumberParserWithResult.toInt("42")
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow()).isEqualTo(42)
+    }
+
+    @Test
+    fun `should parse a double value`() {
+        val result = NumberParserWithResult.toInt("42.0")
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow()).isEqualTo(42)
+    }
+
+    @Test
+    fun `should accept a comma as decimal point`() {
+        val result = NumberParserWithResult.toInt("42,0")
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow()).isEqualTo(42)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["42.4", "42.5", "42.6"])
+    fun `should always round a double value up`(str: String) {
+        val result = NumberParserWithResult.toInt(str)
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrThrow()).isEqualTo(43)
+    }
+
+    @Test
+    fun `should throw an exception if int value is too high`() {
+        val invalidValue = Int.MAX_VALUE.toLong() + 1
+        val result = NumberParserWithResult.toInt("$invalidValue")
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()?.message).contains(Int.MAX_VALUE.toString())
+        assertThat(result.exceptionOrNull()?.message).contains(invalidValue.toString())
+    }
+
+    @Test
+    fun `should throw an exception if int value is too low`() {
+        val invalidValue = Int.MIN_VALUE.toLong() - 1
+        val result = NumberParserWithResult.toInt("$invalidValue")
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()?.message).contains(Int.MIN_VALUE.toString())
+        assertThat(result.exceptionOrNull()?.message).contains(invalidValue.toString())
+    }
+
+    @Test
+    fun `should throw an exception if the decimal point is neither dot nor comma`() {
+        val result = NumberParserWithResult.toInt("42;0")
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()).isInstanceOf(NumberFormatException::class.java)
+    }
+
+    @Test
+    fun `should throw an exception if the passed value is not a number`() {
+        val result = NumberParserWithResult.toInt("42.a")
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.exceptionOrNull()).isInstanceOf(NumberFormatException::class.java)
+    }
+}
